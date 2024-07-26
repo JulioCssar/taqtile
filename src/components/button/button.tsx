5602dab242b9f5ab5ee70f0f173b2e948bcf3ea3
JulioCssar <jc661513@gmail.com>
2024-07-25 23:43:31 -0300

    fix: style button

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -2,17 +2,15 @@ import { ButtonHTMLAttributes } from 'react';
 import { theme } from '../typography/theme';
 import { LinkButton } from '../typography/link-button/link-button';
 
-
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
   expanded?: boolean;
 }
 export function Button({ children, expanded, ...rest }: ButtonProps) {
   const buttonStyle = {
-    color: theme.brandSecondary,
     backgroundColor: theme.accessoryPurple,
     border: 'none',
-    padding: '4px 24px',
+    padding: '12px 24px',
     width: expanded ? '100%' : ' ',
     borderRadius: '20px',
     cursor: 'pointer',
