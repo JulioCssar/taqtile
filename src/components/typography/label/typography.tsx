040e10ee689311cb2aabf93104de814451882c57
JulioCssar <jc661513@gmail.com>
2024-07-25 23:40:57 -0300

    fix: typography label

--- src/components/typography/label/typography.tsx
+++ src/components/typography/label/typography.tsx
@@ -3,14 +3,20 @@ import { theme } from '../theme';
 
 interface LabelProps {
   children: string;
+  error?: string;
 }
 
-const LabelStyle = {
-  fontFamily: Constants.fonts.fontSecundary,
-  fontSize: Constants.size.medium,
-  color: theme.brandPrimary,
-  fontWeight: Constants.weight.regular,
-};
-export function Label({ children }: LabelProps) {
-  return <label style={LabelStyle}>{children}</label>;
+
+export function Label({ children, error }: LabelProps) {
+  const LabelStyle = {
+    fontFamily: Constants.fonts.fontSecundary,
+    fontSize: Constants.size.medium,
+    color: error ? theme.statesError: theme.brandPrimary,
+    fontWeight: Constants.weight.regular,
+  };
+  return (
+    <label htmlFor={children} style={LabelStyle}>
+      {children}
+    </label>
+  );
 }
