6e9c6298ab16421e8912928052022930f2c0c00c
JulioCssar <jc661513@gmail.com>
2024-07-25 23:53:14 -0300

    fix: add input refs

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -1,21 +1,23 @@
-import { InputHTMLAttributes } from 'react';
+import { forwardRef, InputHTMLAttributes } from 'react';
 import { theme } from '../typography/theme';
 import './input.css';
 import { Label } from '../typography/label/typography';
+import { Caption } from '../typography/caption/caption';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string;
-  error?: boolean;
+  error?: string;
 }
 
-export function Input({ error, label }: InputProps) {
+export const Input = forwardRef<HTMLInputElement, InputProps>(({ error, label, ...rest }, ref) => {
   const inputStyle = {
     borderColor: error ? theme.statesError : theme.baseGray,
   };
   return (
     <div className='container-input'>
-      <Label>{label}</Label>
-      <input style={inputStyle} className='input' />
+      <Label error={error}>{label}</Label>
+      <input ref={ref} {...rest} style={inputStyle} className='input' />
+      {error && <Caption>{error}</Caption>}
     </div>
   );
-}
+});
